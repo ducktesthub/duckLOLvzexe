8296e79b46b9c438a3a7bb7f3019c5671599400d
ducksaygex <viet1512010@gmail.com>
2025-10-06 18:11:44 +0700

    Update DuckXHub_MAIN.lua

--- DuckXHub_MAIN.lua
+++ DuckXHub_MAIN.lua
@@ -1,2 +1,2 @@
 -- what are you doing here?
-loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/c6125bde9d5c9d3d"))()
+loadstring(game:HttpGet("https://pandadevelopment.net/virtual/file/384d2c014a5370b2"))()
